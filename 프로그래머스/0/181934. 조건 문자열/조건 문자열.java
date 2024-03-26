b60274123480e7b7684aee981110e3d69aa76ed6
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2024-03-26 10:32:55 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,18 +1,29 @@
 class Solution {
         public int solution(String ineq, String eq, int n, int m) {
-           if(ineq.equals(">")){
-               if(eq.equals("=")){
-                  return n >= m ? 1 : 0;
-               } else if (eq.equals("!")){
-                   return n > m ? 1 : 0;
-               }
-           } else if (ineq.equals("<")){
-               if(eq.equals("=")){
-                   return n <=m ? 1 : 0;
-               } else if (eq.equals("!")){
-                   return n < m ? 1 : 0;
-               }
-           }
-           return 0;
+           // if(ineq.equals(">")){
+           //     if(eq.equals("=")){
+           //        return n >= m ? 1 : 0;
+           //     } else if (eq.equals("!")){
+           //         return n > m ? 1 : 0;
+           //     }
+           // } else if (ineq.equals("<")){
+           //     if(eq.equals("=")){
+           //         return n <=m ? 1 : 0;
+           //     } else if (eq.equals("!")){
+           //         return n < m ? 1 : 0;
+           //     }
+           // }
+           // return 0;
+            
+             if(ineq.equals(">") && eq.equals("=")) {
+                return n >= m ? 1 : 0 ;
+            }
+            if(ineq.equals("<") && eq.equals("=")) {
+                return n<=m? 1 : 0 ;
+            }
+            if(ineq.equals(">") && eq.equals("!")) {
+                return n>m? 1 : 0 ;
+            }
+            return n<m? 1 : 0 ;
         }
     }
