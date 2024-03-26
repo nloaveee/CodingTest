f0eb51867712362c466b6e070fab928bcac74267
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2024-03-27 08:44:35 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,17 +1,30 @@
  class Solution {
         public int solution(int[] num_list) {
-            int multiply = 1;
+            //1.
+//             int multiply = 1;
+//             int sum = 0;
+            
+//            for (int i = 0 ; i < num_list.length; i++){
+//                multiply *= num_list[i];
+//                sum = sum + num_list[i];
+//            }
+//            sum = (sum*sum);
+//            if (multiply < sum) {
+//                return 1;
+//            }else {
+//                return 0;
+//            }
+            
+            //2. 
+             int mul = 1;
             int sum = 0;
             
-           for (int i = 0 ; i < num_list.length; i++){
-               multiply *= num_list[i];
-               sum = sum + num_list[i];
-           }
-           sum = (sum*sum);
-           if (multiply < sum) {
-               return 1;
-           }else {
-               return 0;
-           }
+            for (int i = 0 ; i <num_list.length; i++){
+                sum += num_list[i];
+                mul *= num_list[i];
+            }
+            
+            return mul < Math.pow(sum,2) ? 1:0;
+            
         }
     }
