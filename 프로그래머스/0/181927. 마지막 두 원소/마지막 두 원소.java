436b693ab36c6f812614735224c2a329eb97adfe
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2024-03-27 09:26:11 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -22,6 +22,7 @@ import java.util.List;
 
     class Solution {
         public int[] solution(int[] num_list) {
+            // 결과 배열 한칸이 더 필요하기 떄문에 num_list 배열 길이+1
             int[] answer = new int[num_list.length+1];
 
             for(int i = 0; i < num_list.length; i++) {
