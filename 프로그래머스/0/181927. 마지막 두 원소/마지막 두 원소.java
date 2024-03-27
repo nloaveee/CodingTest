e6b9756d8cf8dac7a2e201aa88160ff9d21f3fb6
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2024-03-27 09:25:01 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,21 +1,38 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
-        public List solution(int[] num_list) {
-            List<Integer> answer = new ArrayList<>();
+//    class Solution {
+//        public List solution(int[] num_list) {
+//            List<Integer> answer = new ArrayList<>();
+//
+//            int last = num_list[num_list.length-1];
+//            int lastb = num_list[num_list.length-2];
+//
+//            for (int i = 0; i< num_list.length; i++){
+//                answer.add(num_list[i]);
+//            }
+//            if(last > lastb){
+//                answer.add(last-lastb);
+//            }else {
+//                answer.add(last * 2);
+//            }
+//            return answer;
+//        }
+//    }
+
+    class Solution {
+        public int[] solution(int[] num_list) {
+            int[] answer = new int[num_list.length+1];
+
+            for(int i = 0; i < num_list.length; i++) {
+                answer[i] = num_list[i];
+            }
 
             int last = num_list[num_list.length-1];
-            int lastb = num_list[num_list.length-2];
+            int before = num_list[num_list.length-2];
 
-            for (int i = 0; i< num_list.length; i++){
-                answer.add(num_list[i]);
-            }
-            if(last > lastb){
-                answer.add(last-lastb);
-            }else {
-                answer.add(last * 2);
-            }
+            answer[answer.length-1] = last > before ? last - before : last*2;
+
             return answer;
         }
     }
