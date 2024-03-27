a84a521d164818f76ba8a51de483241e0e569de6
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2024-03-27 18:04:25 +0900

    [level 0] Title: 수 조작하기 2, Time: 683.68 ms, Memory: 372 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,17 +1,45 @@
 class Solution {
         public String solution(int[] numLog) {
+            
+            //1. 
+//            String answer = "";
+//            for (int i = 1; i < numLog.length; i++) {
+//                if (numLog[i] - numLog[i-1] == 1) {
+//                    answer += "w";
+//                } else if (numLog[i] - numLog[i-1] == -1){
+//                    answer += "s";
+//                } else if (numLog[i] - numLog[i-1] == 10){
+//                    answer += "d";
+//                } else if (numLog[i] - numLog[i-1] == -10){
+//                    answer += "a";
+//                }
+//            }
+//            return answer;
+//        }
+
+            
+            //2.
             String answer = "";
             for (int i = 1; i < numLog.length; i++) {
-                if (numLog[i] - numLog[i-1] == 1) {
-                    answer += "w";
-                } else if (numLog[i] - numLog[i-1] == -1){
-                    answer += "s";
-                } else if (numLog[i] - numLog[i-1] == 10){
-                    answer += "d";
-                } else if (numLog[i] - numLog[i-1] == -10){
-                    answer += "a";
+                // 
+                int j = numLog[i - 1] - numLog[i];
+
+                switch (j) {
+                    case -1:
+                        answer += "w";
+                        break;
+                    case 1:
+                        answer += "s";
+                        break;
+                    case -10:
+                        answer += "d";
+                        break;
+                    case 10:
+                        answer += "a";
+                        break;
                 }
             }
+
             return answer;
         }
     }
