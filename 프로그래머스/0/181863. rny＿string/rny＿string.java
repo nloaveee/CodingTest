03de64c321310bc334ec1b64da7073b683fc713f
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2024-04-03 21:35:04 +0900

    [level 0] Title: rny_string, Time: 0.07 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,14 +1,21 @@
-class Solution {
+ class Solution {
         public String solution(String rny_string) {
-            String answer = "";
-            for (int i = 0; i < rny_string.length(); i++) {
-                if (rny_string.contains("m")) {
-                    answer = rny_string.replace("m", "rn");
-                }
-                if (!rny_string.contains("m")) {
-                    return rny_string;
-                }
-            }
-            return answer;
+
+            // 1
+//            String answer = "";
+//            for (int i = 0; i < rny_string.length(); i++) {
+//                if (rny_string.contains("m")) {
+//                    answer = rny_string.replace("m", "rn");
+//                }
+//                if (!rny_string.contains("m")) {
+//                    return rny_string;
+//                }
+//            }
+//            return answer;
+
+
+            // 2
+            return rny_string.replaceAll("m","rn");
+
         }
     }
