f9d701528a3f30d5ab30455d9e3bddabd81169a3
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2024-04-04 11:31:27 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 5.39 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -1,17 +1,30 @@
 class Solution {
-    public int[] solution(int[] arr) {
-        int[] answer = new int[arr.length];
-        
-        for (int i = 0; i < answer.length; i++) {
-            if (arr[i] < 50 && arr[i] % 2 != 0) {
-                answer[i] = arr[i] * 2;
-            } else if (arr[i] >= 50 && arr[i] % 2 == 0) {
-                answer[i] = arr[i] / 2;
-            } else {
-                answer[i] = arr[i];
+        public int[] solution(int[] arr) {
+
+            // 1
+            for (int i = 0 ; i<arr.length; i++) {
+                if(arr[i]>=50 && arr[i]%2 == 0) {
+                    arr[i] = arr[i]/2;
+                }else if (arr[i] <50 && arr[i] %2!=0){
+                    arr[i]= arr[i]*2;
+                }
             }
+            return arr;
+
+
+
+            // 2
+//            int[] answer = new int [arr.length];
+//
+//            for (int i = 0; i < arr.length; i++) {
+//                if (arr[i] >= 50 && arr[i] % 2 == 0) {
+//                    answer[i] = arr[i] / 2;
+//                } else if (arr[i] < 50 && arr[i] % 2 != 0) {
+//                    answer[i] = arr[i] * 2;
+//                } else {
+//                    answer[i]=arr[i];
+//                }
+//            }
+//            return answer;
         }
-        
-        return answer;
     }
-}
