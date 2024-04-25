5c37af5010215be283111f05b32d77fad7da427c
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2024-04-25 10:55:03 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,15 +1,26 @@
-class Solution {
-        public String solution(String myString) {
-            String answer = "";
-            char[] chars = myString.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
-                if (chars[i] == 'a') {
-                    chars[i] = 'A';
-                } else if (chars[i]!='A' && Character.isUpperCase(chars[i])) {
-                    chars[i] = Character.toLowerCase(chars[i]);
-                }
-            }
-            answer = String.valueOf(chars);
-            return answer;
+// 다른 풀이 
+    class Solution {
+        public String solution(String myString){
+            myString =myString.toLowerCase();
+            myString=myString.replace('a','A');
+            return myString;
         }
     }
+    
+//    
+//    //내 풀이 
+//    class Solution {
+//        public String solution(String myString) {
+//            String answer = "";
+//            char[] chars = myString.toCharArray();
+//            for (int i = 0; i < chars.length; i++) {
+//                if (chars[i] == 'a') {
+//                    chars[i] = 'A';
+//                } else if (chars[i]!='A' && Character.isUpperCase(chars[i])) {
+//                    chars[i] = Character.toLowerCase(chars[i]);
+//                }
+//            }
+//            answer = String.valueOf(chars);
+//            return answer;
+//        }
+//    }
