637fa176d7fdb6a28aace1215551db22fde6a750
nloaveee <130201520+nloaveee@users.noreply.github.com>
2024-05-30 15:16:37 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.39 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,16 +1,16 @@
 import java.util.Arrays;   
- class Solution {
+class Solution {
         public long solution(long n) {
 
             String[] str = String.valueOf(n).split("");
-
-            Arrays.sort(str);
+            Arrays.sort(str); // 배열 정렬 
 
             StringBuilder sb = new StringBuilder();
-            for(int i=str.length-1; i >= 0; i--){
+            for(int i=str.length-1; i >= 0; i--){ // 배열의 뒤에서 부터 거꾸로 담는다.
                 sb.append(str[i]);
             }
 
+            // string -> long
             long answer = Long.parseLong(String.valueOf(sb));
 
             return answer;
