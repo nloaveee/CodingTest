a711641de903c43f7c18445101363b5aba90dc74
nloaveee <130201520+nloaveee@users.noreply.github.com>
2024-06-13 11:05:07 +0900

    [level 2] Title: 숫자의 표현, Time: 0.10 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,6 +1,7 @@
-class Solution {
-    public int solution(int n) {
-        int answer = 0;
+
+       class Solution {
+        public int solution(int n) {
+            int answer = 0;
 
             for(int i = 1; i<=n; i+=2){
                 if(n%i==0){
@@ -8,5 +9,27 @@ class Solution {
                 }
             }
             return answer;
+        }
     }
-}
+
+    
+
+
+//    public static void main(String[] args) {
+//        System.out.println(solution(15));
+//    }
+//        public static int solution(int n) {
+//
+//            int cnt=0;
+//            for(int i=1; i<=n; i++){
+//                int sum=0;
+//                for(int j=i; j<=n;j++){
+//                    sum+=j;
+//                    if(sum == n){
+//                        cnt++;
+//                        continue;
+//                    }
+//                }
+//            }
+//            return cnt;
+//        }
