6d6652572335d24ac8347e64d66cdc366a1457af
nloaveee <130201520+nloaveee@users.noreply.github.com>
2024-06-14 12:17:09 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-            for (int i = 0; i < number.length; i++) {
-                for (int j = i+1; j < number.length; j++) {
+            for (int i = 0; i < number.length-2; i++) {
+                for (int j = i+1; j < number.length-1; j++) {
                     for (int k = j+1; k < number.length; k++) {
                         if (number[i] + number[j] + number[k]   == 0) {
                             answer++;
