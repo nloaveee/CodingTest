64d1faa52fc45c6c43bd9ec20381a9ce8ed73c14
nloaveee <130201520+nloaveee@users.noreply.github.com>
2024-10-02 13:11:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.08 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,18 +1,44 @@
 import java.util.*;
 class Solution {
         public String solution(String[] participant, String[] completion) {
-            // 1. 배열 정렬
-            Arrays.sort(participant);
-            Arrays.sort(completion);
 
-            // 2. 비교
-            int i=0;
-            for (; i<completion.length; i++) {
-                if (!completion[i].equals(participant[i])){
+            String answer = "";
+            // 1. hash에 넣기
+            Map<String, Integer> map = new HashMap<>();
+
+            for (String human : participant) {
+                map.put(human,map.getOrDefault(human,0)+1);
+            }
+
+            // 2. completion 값 빼기
+            for (String human : completion) {
+                map.put(human,map.get(human)-1);
+            }
+
+            // 3. value 가 0이 아닌 값 가져오기
+            for (String key : map.keySet()) {
+                if (map.get(key) != 0) {
+                    answer = key;
                     break;
                 }
             }
 
-            return participant[i];
+            return answer;
+
+
+
+//            // 1. 배열 정렬
+//            Arrays.sort(participant);
+//            Arrays.sort(completion);
+//
+//            // 2. 비교
+//            int i=0;
+//            for (; i<completion.length; i++) {
+//                if (!completion[i].equals(participant[i])){
+//                    break;
+//                }
+//            }
+//
+//            return participant[i];
         }
     }
