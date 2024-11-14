84d0f7c9f8a121f591f8d3f132fc0662a7160d27
nloaveee <130201520+nloaveee@users.noreply.github.com>
2024-11-14 12:48:02 +0900

    [level 1] Title: 폰켓몬, Time: 5.67 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i< nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        if (nums.length/2 >= set.size()) {
+            answer = set.size();
+        } else {
+            answer = nums.length/2;
+        }
+        
+        return answer;
+    }
+}
