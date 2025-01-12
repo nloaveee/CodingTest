9dd7834ac2b0e459035252965e89b3e39c66f2c5
nloaveee <130201520+nloaveee@users.noreply.github.com>
2025-01-12 12:33:11 +0900

    [level 1] Title: 3진법 뒤집기, Time: 12.18 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        
+        String str = "";
+        
+        while(n > 0) {
+            str += n % 3;
+            n = n / 3;
+        }
+        
+        return Integer.parseInt(str,3);
+        
+    }
+}
