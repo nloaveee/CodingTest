ee1569e20cb7957e71513c259611eecf6ead8611
nloaveee <130201520+nloaveee@users.noreply.github.com>
2025-01-16 11:19:26 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.22 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+
+        List<String> list = new ArrayList<>();
+        list.add(words[0]);
+
+        for(int i=1; i<words.length; i++){
+            if(list.contains(words[i]) || same(list.get(list.size()-1), words[i])){
+                answer[0] = i%n+1;
+                answer[1] = i/n+1;
+                break;
+            }
+            list.add(words[i]);
+        }
+
+        return answer;
+    }
+
+    public boolean same(String word1, String word2) {
+        if(word1.charAt(word1.length()-1) == word2.charAt(0)){
+            return false;
+        }
+        return true;
+    }
+}
