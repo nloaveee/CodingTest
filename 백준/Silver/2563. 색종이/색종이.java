6b138d816b8d13cdb0d06dc55d63819f64ec4d88
nloaveee <130201520+nloaveee@users.noreply.github.com>
2025-02-06 10:26:07 +0900

    [Silver V] Title: 색종이, Time: 176 ms, Memory: 17776 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,35 +1,31 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+
+        boolean[][] paper = new boolean[101][101];
         Scanner scanner = new Scanner(System.in);
 
-        int[][] paper = new int[100][100];
-        int count = scanner.nextInt();
+        int N = scanner.nextInt();
+        int total = 0;
 
-        // count 수 만큼 입력받기
-        for (int i = 0; i < count; i++) {
-            int a = scanner.nextInt(); // 열 가로
-            int b = scanner.nextInt(); // 행 세로
-            for (int j = a; j < a + 10; j++) {
-            // 사각형 부분에 1을 넣어주기
-                for (int k = b; k < b + 10; k++) {
-                    paper[k][j] = 1;
-                }
-            }
+        for (int i = 0; i<N; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
 
-        }
-
-        // 2차원 배열 출력. 1인 숫자만 더하기.
-        int res = 0;
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (paper[i][j] == 1) {
-                    res += paper[i][j];
+            for (int j = x; j < x+10; j++) {
+                for (int z = y; z < y+10; z++) {
+                    if (!paper[j][z]) {
+                        paper[j][z] = true;
+                        total++;
+                    }
                 }
             }
         }
-        System.out.println(res);
 
+        System.out.println(total);
     }
+
 }
